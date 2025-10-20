efca0df788c042759156eed15a102f2f308d1019
Johannes <johannes@soundofsynergy.com>
2025-10-20 23:12:26 +0200

    Add Transperency for Trees and Deco when touching

---
+++ Assets/Scripts/HideLayer.cs
@@ -0,0 +1,46 @@
+using System;
+using UnityEngine;
+using UnityEngine.Tilemaps;
+
+public class HideLayer : MonoBehaviour
+{
+    [SerializeField] private float fullVisibility;
+    [SerializeField] private float transparency;
+
+    private bool playerHovering = false;
+
+    private void Start()
+    {
+        Show();
+    }
+    
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            Hide();
+            playerHovering = true;
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            Show();
+            playerHovering = false;
+        }
+    }
+
+    public void Hide()
+    {
+        var color = new Color(1.0f, 1.0f, 1.0f, transparency);
+        this.GetComponent<Tilemap>().color = color;
+    }
+
+    public void Show()
+    {
+        var color = new Color(1.0f, 1.0f, 1.0f, fullVisibility);
+        this.GetComponent<Tilemap>().color = color;
+    }
+}
